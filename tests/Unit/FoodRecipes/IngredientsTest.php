529103fa08ca56fd4e14190adde2d33a028b9d7a
przper <avan.radien@gmail.com>
2024-02-23 23:31:55 +0100

    WIP UnitTest Ingredients

--- tests/Unit/FoodRecipes/IngredientsTest.php
+++ tests/Unit/FoodRecipes/IngredientsTest.php
@@ -3,8 +3,21 @@
 namespace Tests\Unit\FoodRecipes;
 
 use PHPUnit\Framework\TestCase;
+use Przper\Tribe\FoodRecipes\Domain\Ingredients;
+use Przper\Tribe\FoodRecipes\Domain\Ingredient;
+use Przper\Tribe\FoodRecipes\Domain\Name;
 
 class IngredientsTest extends TestCase
 {
-    
+    public function test_it_can_add_Ingredients()
+    {
+        $ingredients = new Ingredients();
+
+        $ingredient = new Ingredient();
+        $ingredient->name = new Name("New Ingredient");
+
+        $ingredients->add($ingredient);
+
+        $this->assertSame($ingredients->ingredients[0]->name->value, "New Ingredient");
+    }
 }
