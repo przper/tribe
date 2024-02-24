77a506de3d492fb78977321ddbe2c56b9574573d
przper <avan.radien@gmail.com>
2024-02-25 00:09:54 +0100

    wip AggregateRoot

--- src/FoodRecipes/Domain/Recipe.php
+++ src/FoodRecipes/Domain/Recipe.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Przper\Tribe\FoodRecipes\Domain;
 
-final class Recipe
+final class Recipe extends AggregateRoot
 {
     private Ingredients $ingredients;
 
---
+++ src/Shared/Domain/AggregateRoot.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Przper\Tribe\Shared\Domain;
+
+abstract class AggregateRoot
+{
+    
+}
