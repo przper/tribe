8c6bc3ebdf6d3de9e1bc2797ad701236546b909f
przper <avan.radien@gmail.com>
2024-02-25 23:52:31 +0100

    dbal playground

--- playground_database.php
+++ playground_database.php
@@ -1,5 +1,18 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\DsnParser;
+
 require 'index.php';
 
-echo $_ENV['DATABASE_URL'];
+$connectionParams = (new DsnParser())->parse($_ENV['DATABASE_URL']);
+echo json_encode($connectionParams)."\n";
+
+$connection = DriverManager::getConnection($connectionParams);
+
+$sql = "SELECT * FROM test";
+$statement = $connection->prepare($sql);
+
+$result = $statement->executeQuery();
+
+echo json_encode($result)."\n";
