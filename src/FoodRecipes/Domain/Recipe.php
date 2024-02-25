ecfcce5a1152e53652eb5d4a695a53c8c08ecc38
przper <avan.radien@gmail.com>
2024-02-25 23:08:13 +0100

    [fix] added AggregateRoot import to Recipe

--- src/FoodRecipes/Domain/Recipe.php
+++ src/FoodRecipes/Domain/Recipe.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Przper\Tribe\FoodRecipes\Domain;
 
+use Przper\Tribe\Shared\Domain\AggregateRoot;
+
 final class Recipe extends AggregateRoot
 {
     private Ingredients $ingredients;
