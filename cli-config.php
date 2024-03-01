7b0a438dcc73cb9a1e40146fb78fd2f9093f6523
przper <avan.radien@gmail.com>
2024-03-02 00:06:24 +0100

    Installed `doctrine/migrations`

---
+++ cli-config.php
@@ -0,0 +1,18 @@
+<?php
+
+# This is script for `vendor/bin/doctrine-migrations` to run
+
+require 'index.php';
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\DsnParser;
+use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
+use Doctrine\Migrations\Configuration\Migration\YamlFile;
+use Doctrine\Migrations\DependencyFactory;
+
+$config = new YamlFile('migrations.yaml');
+
+$connectionParams = (new DsnParser())->parse($_ENV['DATABASE_URL']);
+$connection = DriverManager::getConnection($connectionParams);
+
+return DependencyFactory::fromConnection($config, new ExistingConnection($connection));
---
+++ migrations/Version20240301223004.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tribe\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240301223004 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create Recipe table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            CREATE TABLE `recipe` (
+                `id`    VARCHAR(255) DEFAULT (UUID()) PRIMARY KEY,
+                `name`  VARCHAR(255) NOT NULL
+            );
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            DROP TABLE IF EXISTS recipe;
+        SQL);
+    }
+}
