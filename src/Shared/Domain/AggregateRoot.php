d7c0147d40a3e341bdb8c0ac9f81a9374e9c5d2d
przper <avan.radien@gmail.com>
2024-03-02 23:44:43 +0100

    Added `pullEvents` and `raise` methods to AggregateRoot

--- src/Shared/Domain/AggregateRoot.php
+++ src/Shared/Domain/AggregateRoot.php
@@ -2,4 +2,20 @@
 
 namespace Przper\Tribe\Shared\Domain;
 
-abstract class AggregateRoot {}
+abstract class AggregateRoot
+{
+    protected array $events = [];
+
+    final public function pullEvents(): array
+    {
+        $events = $this->events;
+        $this->events = [];
+
+        return $events;
+    }
+
+    final protected function raise(DomainEvent $event): void
+    {
+        $this->events[] = $event;
+    }
+}
