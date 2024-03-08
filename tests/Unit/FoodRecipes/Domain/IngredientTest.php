ad3b36b9d9bc486f6ff0cead552dbb77709fa9f7
Przemysław Perzyna <przemyslaw.perzyna@polcode.net>
2024-03-08 23:01:39 +0100

    Reorganized tests dir

--- tests/Unit/FoodRecipes/Domain/IngredientTest.php
+++ tests/Unit/FoodRecipes/Domain/IngredientTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit\FoodRecipes;
+namespace FoodRecipes\Domain;
 
 use PHPUnit\Framework\TestCase;
 use Przper\Tribe\FoodRecipes\Domain\Ingredient;
--- tests/Unit/FoodRecipes/Domain/IngredientsTest.php
+++ tests/Unit/FoodRecipes/Domain/IngredientsTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Tests\Unit\FoodRecipes;
+namespace FoodRecipes\Domain;
 
 use PHPUnit\Framework\TestCase;
-use Przper\Tribe\FoodRecipes\Domain\Ingredients;
 use Przper\Tribe\FoodRecipes\Domain\Ingredient;
+use Przper\Tribe\FoodRecipes\Domain\Ingredients;
 use Przper\Tribe\FoodRecipes\Domain\Name;
 
 class IngredientsTest extends TestCase
