2d55340e0bd67ca2c4753f45088f11b977c461d6
przper <avan.radien@gmail.com>
2024-06-11 22:28:57 +0200

    Formatted example data in PolcodeLinkWorkedTimeRetrieverTest

--- tests/Unit/WorkedTime/Unit/Infrastructure/PolcodeLinkWorkedTimeRetrieverTest.php
+++ tests/Unit/WorkedTime/Unit/Infrastructure/PolcodeLinkWorkedTimeRetrieverTest.php
@@ -16,106 +16,72 @@ class PolcodeLinkWorkedTimeRetrieverTest extends TestCase
         $linkApiKey = 'test link api key';
         $mockedHttpClient = $this->createMock(Client::class);
         $workedHoursDataMap = [
-            array(0) {
-    }
-array(0) {
-    }
-array(1) {
-        [0]=>
-  array(7) {
-            ["id"]=>
-    int(120095)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-            ["fromDate"]=>
-    string(25) "2024-06-03T07:40:00+02:00"
-            ["toDate"]=>
-    string(25) "2024-06-03T15:54:00+02:00"
-            ["comment"]=>
-    string(19) "VS-3845, 2716, 3846"
-            ["minutes"]=>
-    int(494)
-  }
-}
-array(2) {
-        [0]=>
-  array(7) {
-            ["id"]=>
-    int(120212)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-            ["fromDate"]=>
-    string(25) "2024-06-04T07:46:00+02:00"
-            ["toDate"]=>
-    string(25) "2024-06-04T12:03:00+02:00"
-            ["comment"]=>
-    string(7) "VS-3847"
-            ["minutes"]=>
-    int(257)
-  }
-  [1]=>
-  array(7) {
-            ["id"]=>
-    int(120213)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-            ["fromDate"]=>
-    string(25) "2024-06-04T13:10:00+02:00"
-            ["toDate"]=>
-    string(25) "2024-06-04T16:35:00+02:00"
-            ["comment"]=>
-    string(7) "VS-3847"
-            ["minutes"]=>
-    int(205)
-  }
-}
-array(1) {
-        [0]=>
+            [], //saturday
+            [], //sunday
             [
-                'id' => 120351,
-                'categoryId' => 434,
-                'categoryName' => 'Vertuoza',
-                'fromDate' => '2024-06-05T07:40:00+02:00',
-                'toDate' => '2024-06-05T16:08:00+02:00',
-                'comment' => "VS-3847",
-                'minutes' => 508,
-            ]
-}
-array(1) {
-        [0]=>
-  array(7) {
-            ["id"]=>
-    int(120758)
-    ["categoryId"]=>
-    int(434)
-        "fromDate":"2024-06-06T08:18:00+02:00"
-        "toDate":"2024-06-06T16:13:00+02:00"
-            ["comment"]=>
-    string(7) "VS-3847"
-            ["minutes"]=>
-    int(475)
-  }
-}
-    [],
-    [],
-    [],
-    [],
-
+                [
+                    'id' => 120095,
+                    'categoryId' => 434,
+                    'categoryName' => 'Vertuoza',
+                    'fromDate' => '2024-06-03T07:40:00+02:00',
+                    'toDate' => '2024-06-03T15:54:00+02:00',
+                    'comment' => "VS-3845, 2716, 3846",
+                    'minutes' => 494,
+                ],
+            ],
+            [
+                [
+                    'id' => 120212,
+                    'categoryId' => 434,
+                    'categoryName' => 'Vertuoza',
+                    'fromDate' => '2024-06-04T07:46:00+02:00',
+                    'toDate' => '2024-06-04T12:03:00+02:00',
+                    'comment' => "VS-3847",
+                    'minutes' => 257,
+                ],
+                [
+                    'id' => 120213,
+                    'categoryId' => 434,
+                    'categoryName' => 'Vertuoza',
+                    'fromDate' => '2024-06-04T13:10:00+02:00',
+                    'toDate' => '2024-06-04T16:35:00+02:00',
+                    'comment' => "VS-3847",
+                    'minutes' => 205,
+                ],
+            ],
+            [
+                [
+                    'id' => 120351,
+                    'categoryId' => 434,
+                    'categoryName' => 'Vertuoza',
+                    'fromDate' => '2024-06-05T07:40:00+02:00',
+                    'toDate' => '2024-06-05T16:08:00+02:00',
+                    'comment' => "VS-3847",
+                    'minutes' => 508,
+                ],
+            ],
+            [
+                [
+                    'id' => 120758,
+                    'categoryId' => 434,
+                    'categoryName' => 'Vertuoza',
+                    'fromDate' => '2024-06-06T08:18:00+02:00',
+                    'toDate' => '2024-06-06T16:13:00+02:00',
+                    'comment' => "VS-3847",
+                    'minutes' => 475,
+                ],
+            ],
+            [], //day off
+            [], //saturday
+            [], //sunday
         ];
 
         $mockedHttpClient
             ->method('request')
-            ->willReturnOnConsecutiveCalls(
-                new Response(body: json_encode($workedHoursDataMap['2000-01-01'])),
-                new Response(body: json_encode($workedHoursDataMap['2000-01-02'])),
-                new Response(body: json_encode($workedHoursDataMap['2000-01-03'])),
-            );
+            ->willReturnOnConsecutiveCalls(...array_map(
+                fn (array $i): Response => new Response(body: json_encode($i)),
+                $workedHoursDataMap,
+            ));
 
         $retriever = new PolcodeLinkWorkedTimeRetriever($linkApiKey, $mockedHttpClient);
 
@@ -124,108 +90,7 @@ array(1) {
             new \DateTimeImmutable('2024-06-10')
         );
 
-        $this->assertSame("24:00", (string) $workingMonth->getExpectedWorkedTimeDuration());
-        $this->assertSame("23:06", (string) $workingMonth->getTotalWorkedTimeDuration());
+        $this->assertSame("32:00", (string) $workingMonth->getExpectedWorkedTimeDuration());
+        $this->assertSame("32:19", (string) $workingMonth->getTotalWorkedTimeDuration());
     }
-}
-
-array(0) {
-}
-array(0) {
-}
-array(1) {
-    [0]=>
-  array(7) {
-        ["id"]=>
-    int(120095)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-        ["fromDate"]=>
-    string(25) "2024-06-03T07:40:00+02:00"
-        ["toDate"]=>
-    string(25) "2024-06-03T15:54:00+02:00"
-        ["comment"]=>
-    string(19) "VS-3845, 2716, 3846"
-        ["minutes"]=>
-    int(494)
-  }
-}
-array(2) {
-    [0]=>
-  array(7) {
-        ["id"]=>
-    int(120212)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-        ["fromDate"]=>
-    string(25) "2024-06-04T07:46:00+02:00"
-        ["toDate"]=>
-    string(25) "2024-06-04T12:03:00+02:00"
-        ["comment"]=>
-    string(7) "VS-3847"
-        ["minutes"]=>
-    int(257)
-  }
-  [1]=>
-  array(7) {
-        ["id"]=>
-    int(120213)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-        ["fromDate"]=>
-    string(25) "2024-06-04T13:10:00+02:00"
-        ["toDate"]=>
-    string(25) "2024-06-04T16:35:00+02:00"
-        ["comment"]=>
-    string(7) "VS-3847"
-        ["minutes"]=>
-    int(205)
-  }
-}
-array(1) {
-    [0]=>
-  array(7) {
-        ["id"]=>
-    int(120351)
-    ["categoryId"]=>
-    int(434)
-    ["categoryName"]=>
-    string(8) "Vertuoza"
-        ["fromDate"]=>
-    string(25) "2024-06-05T07:40:00+02:00"
-        ["toDate"]=>
-    string(25) "2024-06-05T16:08:00+02:00"
-        ["comment"]=>
-    string(7) "VS-3847"
-        ["minutes"]=>
-    int(508)
-  }
-}
-array(1) {
-    [0]=>
-  array(7) {
-        ["id"]=>
-    int(120758)
-    ["categoryId"]=>
-    int(434)
-    string(25) "2024-06-06T16:13:00+02:00"
-        ["comment"]=>
-    string(7) "VS-3847"
-        ["minutes"]=>
-    int(475)
-  }
-}
-array(0) {
-}
-array(0) {
-}
-array(0) {
-}
-array(0) {
 }
