8baf7333a2ae425fd20817b828f1672329f741e7
Przemysław Perzyna <przemyslaw.perzyna@polcode.net>
2024-06-27 09:30:23 +0200

    added `GetRecipe` query

---
+++ src/FoodRecipes/Infrastructure/DBAL/Query/GetRecipe.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Przper\Tribe\FoodRecipes\Infrastructure\DBAL\Query;
+
+use Przper\Tribe\FoodRecipes\Application\Query\GetRecipe as GetRecipeQuery;
+use Przper\Tribe\FoodRecipes\Application\Query\Result\Recipe;
+use Przper\Tribe\FoodRecipes\Domain\RecipeId;
+use Przper\Tribe\FoodRecipes\Infrastructure\DBAL\Repository\RecipeRepository;
+
+final class GetRecipe implements GetRecipeQuery
+{
+    public function __construct(
+        private readonly RecipeRepository $recipeRepository,
+    ) {
+    }
+
+    public function execute(RecipeId $id): ?Recipe
+    {
+        $recipe = $this->recipeRepository->get($id);
+
+        return $recipe === null ? null : Recipe::fromDomainModel($recipe);
+    }
+}
