05957dec956ba06556f58cc8e1196968a243c8f5
Przemysław Perzyna <przemyslaw.perzyna@polcode.net>
2024-06-28 13:28:32 +0200

    PolcodeTimeController

---
+++ src/WorkedTime/Ports/Controller/PolcodeTimeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Przper\Tribe\WorkedTime\Ports\Controller;
+
+use Przper\Tribe\WorkedTime\Infrastructure\PolcodeLinkWorkedTimeRetriever;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PolcodeTimeController extends AbstractController
+{
+    public function __construct(
+        private readonly PolcodeLinkWorkedTimeRetriever $timeRetriever,
+    ) {
+    }
+
+    #[Route('/worked-time/polcode')]
+    public function __invoke(): Response
+    {
+        $start = new \DateTimeImmutable('first day of this month');
+        $end = new \DateTimeImmutable('last day of this month');
+
+        $workingMonth = $this->timeRetriever->retrieve($start, $end);
+
+        $timeDifference = $workingMonth->getTotalWorkedTimeDuration()->difference($workingMonth->getExpectedWorkedTimeDuration());
+
+        $message = $workingMonth->getTotalWorkedTimeDuration()->isGreaterThan($workingMonth->getExpectedWorkedTimeDuration())
+            ? "* You are $timeDifference ahead"
+            : "* You are $timeDifference behind"
+        ;
+        return new Response($message);
+    }
+}
