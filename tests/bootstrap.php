0b4c64ea8da81212b39783637bcd5b432a3f73c1
przper <przper@users.noreply.github.com>
2024-07-11 22:22:16 +0000

    Apply PHP CS Fixer Changes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,9 +12,9 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 }
 
 /**
- * Workaround to fix "Test code or tested code did not remove its own exception 
+ * Workaround to fix "Test code or tested code did not remove its own exception
  * handlers" error message after Integration tests
- * 
+ *
  * see: https://github.com/symfony/symfony/issues/53812
  */
 set_exception_handler([new ErrorHandler(), 'handleException']);
