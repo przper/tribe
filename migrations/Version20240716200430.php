31c524c5237e84eac4393361c4232e81a0820ac2
przper <avan.radien@gmail.com>
2024-07-16 23:28:40 +0200

    Ingredients table migration

---
+++ migrations/Version20240716200430.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tribe\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240716200430 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create Ingredient table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+                CREATE TABLE `ingredient` (
+                    `id`        VARCHAR(255) DEFAULT (UUID()) PRIMARY KEY,
+                    `recipe_id` VARCHAR(255) NOT NULL,
+                    `name`      VARCHAR(255) NOT NULL,
+                    `quantity`  DECIMAL(5,3) UNSIGNED NOT NULL,
+                    `unit`      VARCHAR(255) NOT NULL,
+                    CONSTRAINT `fk_ingredient_recipe`
+                        FOREIGN KEY (recipe_id) REFERENCES recipe (id)
+                        ON DELETE CASCADE
+                );
+            SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+                DROP TABLE IF EXISTS ingredient;
+            SQL);
+    }
+}
