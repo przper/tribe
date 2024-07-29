a78ab2d47c4e43ce943df961f906a7dc88418fdc
Przemysław Perzyna <przemyslaw.perzyna@polcode.net>
2024-07-29 16:03:52 +0200

    Remade RecipeIndex in Twig

--- src/FoodRecipes/Ports/Controller/RecipeIndexController.php
+++ src/FoodRecipes/Ports/Controller/RecipeIndexController.php
@@ -3,7 +3,6 @@
 namespace Przper\Tribe\FoodRecipes\Ports\Controller;
 
 use Przper\Tribe\FoodRecipes\Application\Query\GetRecipes;
-use Przper\Tribe\FoodRecipes\Application\Query\Result\RecipeIndex;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,30 +18,8 @@ class RecipeIndexController extends AbstractController
     {
         $recipes = $this->getRecipesQuery->execute();
 
-        $recipesHtml = $recipes !== []
-            ? implode("\n", array_map(fn(RecipeIndex $r) => "<li>{$r->name} <a href='/recipe/{$r->id}'>Show</a></li>", $recipes))
-            : "List is empty :( . Add something!"
-        ;
-
-        $html = <<<HTML
-                <html>
-                    <head></head>
-                    <body>
-                        <div>
-                            <h1>Our tasty recipes:</h1>
-                            
-                            <div><a href="/recipe/new">Add new</a></div>
-                            
-                            <div>
-                                <ol>
-                                    $recipesHtml
-                                </ol>
-                            </div>
-                        </div>
-                    </body>
-                </html>
-            HTML;
-
-        return new Response($html);
+        return $this->render('food_recipes/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
     }
 }
