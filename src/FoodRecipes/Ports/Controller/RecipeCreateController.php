0e700f3fa99d5c23d1dad985083b0564e220ef64
Przemyslaw Perzyna <przemyslaw.perzyna@gmail.com>
2024-07-30 06:20:47 -0700

    Remade RecipeCreate page in twig

--- src/FoodRecipes/Ports/Controller/RecipeCreateController.php
+++ src/FoodRecipes/Ports/Controller/RecipeCreateController.php
@@ -20,71 +20,17 @@ class RecipeCreateController extends AbstractController
     public function __invoke(Request $request): Response
     {
         if ($request->getMethod() === 'POST') {
+            dump($request->getPayload());
             call_user_func($this->createRecipeHandler, new CreateRecipeCommand(
                 $request->get('name'),
                 $request->get('ingredients'),
             ));
 
-            return new RedirectResponse("/recipe");
+            // return new RedirectResponse("/recipe");
         }
 
-        return new Response(<<<HTML
-                <html>
-                    <head></head>
-                    <body>
-                        <div>
-                            <h1>New Recipe</h1>
-                            
-                            <form method="POST">
-                                <div>
-                                    <label for="name">Name</label>
-                                    <input name="name">
-                                </div>
-                                
-                                <div id="#ingredients">
-                                    <label>Ingredients</label>
-                                    <div>
-                                        <div>#1</div>
-                                        <div>
-                                            <label>Name</label>
-                                            <input name="ingredients[0][name]">
-                                        </div>
-                                        <div>
-                                            <label>Quantity</label>
-                                            <input name="ingredients[0][quantity]">
-                                        </div>
-                                        <div>
-                                            <label>Unit</label>
-                                            <input name="ingredients[0][unit]">
-                                        </div>
-                                    </div>
-                                    
-                                    <div>
-                                        <div>#2</div>
-                                        <div>
-                                            <label>Name</label>
-                                            <input name="ingredients[1][name]">
-                                        </div>
-                                        <div>
-                                            <label>Quantity</label>
-                                            <input name="ingredients[1][quantity]">
-                                        </div>
-                                        <div>
-                                            <label>Unit</label>
-                                            <input name="ingredients[1][unit]">
-                                        </div>
-                                    </div>
-                                    
-                                    <div>
-                                        <button type="button">Add one more ingredient (soon)</button>
-                                    </div>
-                                </div>
-                                
-                                <button type="submit">Save</button>
-                            </form>
-                        </div>
-                    </body>
-                </html>
-            HTML);
+        return $this->render(
+            'food_recipes/create.html.twig',
+        );
     }
 }
