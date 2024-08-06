90caff8ff3578e6ff9f5c0452f9851d451b2ab29
Przemyslaw Perzyna <przemyslaw.perzyna@gmail.com>
2024-08-06 15:55:27 +0200

    Created RecipeMother for test purposes

---
+++ tests/Doubles/MotherObjects/RecipeMother.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Doubles\MotherObjects;
+
+use Przper\Tribe\FoodRecipes\Domain\Name;
+use Przper\Tribe\FoodRecipes\Domain\Recipe;
+use Przper\Tribe\FoodRecipes\Domain\RecipeId;
+use Przper\Tribe\Shared\Infrastructure\Ramsey\IdGenerator;
+
+class RecipeMother
+{
+    private Recipe $recipe;
+    private IdGenerator $idGenerator;
+
+    private function __construct()
+    {
+        $this->idGenerator = new IdGenerator();
+        $this->recipe = Recipe::create(
+            new RecipeId($this->idGenerator->generate()),
+            Name::fromString('Test 123'),
+        );
+    }
+
+    public static function new(): self
+    {
+        return new self();
+    }
+
+    public function build(): Recipe
+    {
+        return clone $this->recipe;
+    }
+}
