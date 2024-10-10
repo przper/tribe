29fa1ff4b82b1c7d7a223679fd557e088c41a639
Przemyslaw Perzyna <avan.radien@gmail.com>
2024-10-10 19:59:31 +0200

    Move Projector tests to Unit

---
+++ tests/Unit/FoodRecipes/Application/Projection/ProjectRecipeCreatedTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\FoodRecipes\Application\Projection;
+
+use PHPUnit\Framework\TestCase;
+
+class ProjectRecipeCreatedTest extends TestCase
+{
+    public function test_it_persists_projection(): void
+    {
+
+//        $recipeId = (string) $recipeSaved->getId();
+//        $indexProjection = $projection->getIndexProjection($recipeId);
+//        $this->assertNotNull($indexProjection);
+//        $this->assertIsArray($indexProjection);
+//        $this->assertArrayHasKey('id', $indexProjection);
+//        $this->assertArrayHasKey('name', $indexProjection);
+//        $this->assertSame('Chilli con Carne', $indexProjection['name']);
+//
+//        $detailProjection = $projection->getDetailProjection($recipeId);
+//        $this->assertNotNull($detailProjection);
+//        $this->assertIsArray($detailProjection);
+//        $this->assertArrayHasKey('id', $detailProjection);
+//        $this->assertArrayHasKey('name', $detailProjection);
+//        $this->assertSame('Chilli con Carne', $detailProjection['name']);
+//        $this->assertArrayHasKey('ingredients', $detailProjection);
+//        $this->assertCount(2, $detailProjection['ingredients']);
+//        $this->assertSame('Pork: 1 kilogram', $detailProjection['ingredients'][0]);
+//        $this->assertSame('Tomatoes: 3 can', $detailProjection['ingredients'][1]);
+    }
+}
