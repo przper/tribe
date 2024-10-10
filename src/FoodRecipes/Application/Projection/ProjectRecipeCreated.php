04c4cab0bbd9fac8825cd881a7bac8d141f0fec8
Przemyslaw Perzyna <avan.radien@gmail.com>
2024-10-10 20:14:31 +0200

    Change repository dependency of ProjectRecipeCreated

--- src/FoodRecipes/Application/Projection/ProjectRecipeCreated.php
+++ src/FoodRecipes/Application/Projection/ProjectRecipeCreated.php
@@ -5,14 +5,14 @@ namespace Przper\Tribe\FoodRecipes\Application\Projection;
 use Przper\Tribe\FoodRecipes\Domain\Ingredient;
 use Przper\Tribe\FoodRecipes\Domain\RecipeCreated;
 use Przper\Tribe\FoodRecipes\Domain\RecipeId;
-use Przper\Tribe\FoodRecipes\Infrastructure\DBAL\Repository\RecipeRepository;
+use Przper\Tribe\FoodRecipes\Domain\RecipeRepositoryInterface;
 use Przper\Tribe\Shared\Domain\DomainEvent;
 use Przper\Tribe\Shared\Domain\DomainEventListenerInterface;
 
 class ProjectRecipeCreated implements DomainEventListenerInterface
 {
     public function __construct(
-        private readonly RecipeRepository $repository,
+        private readonly RecipeRepositoryInterface $repository,
         private readonly RecipeProjection $projection,
     ) {}
 
