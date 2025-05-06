948730a54959aed432e3e5f654336d3b70587581
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-05-06 21:57:03 +0200

    Add modal for adding Recipe to GroceryList

--- src/FoodRecipes/Ports/Web/RecipeDetailController.php
+++ src/FoodRecipes/Ports/Web/RecipeDetailController.php
@@ -4,16 +4,18 @@ namespace Przper\Tribe\FoodRecipes\Ports\Web;
 
 use Przper\Tribe\FoodRecipes\Application\Query\GetRecipeDetail;
 use Przper\Tribe\FoodRecipes\Application\Query\Result\RecipeDetail;
-use Przper\Tribe\Provisioning\Application\Query\GetGroceryLists;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class RecipeDetailController extends AbstractController
 {
     public function __construct(
         private readonly GetRecipeDetail $getRecipeDetailQuery,
-        private readonly GetGroceryLists $getGroceryListsQuery,
+        #[Autowire(service: 'tribe.internal_http_client')]
+        private readonly HttpClientInterface $httpClient,
     ) {}
 
     #[Route('/recipe/{id}', name: 'recipe_detail', methods: ['GET'])]
@@ -25,9 +27,11 @@ class RecipeDetailController extends AbstractController
             throw $this->createNotFoundException();
         }
 
+        $groceryLists = $this->httpClient->request('GET', '/api/v1/groceries');
+
         return $this->render('food_recipes/detail.html.twig', [
             'recipe' => $recipe,
-            'groceryLists' => $this->getGroceryListsQuery->execute(),
+            'groceryLists' => json_decode($groceryLists->getContent(), true),
         ]);
     }
 }
---
+++ src/Provisioning/Ports/Api/GroceryListActionController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Przper\Tribe\Provisioning\Ports\Api;
+
+use Przper\Tribe\Provisioning\Application\Command\AddRecipeToGroceryList\AddRecipeToGroceryListCommand;
+use Przper\Tribe\Provisioning\Application\Command\AddRecipeToGroceryList\AddRecipeToGroceryListHandler;
+use Przper\Tribe\Provisioning\Application\Query\GetGroceryList;
+use Przper\Tribe\Provisioning\Application\Query\Result\GroceryList;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GroceryListActionController extends AbstractController
+{
+    final public const string ADD_RECIPE_TO_GROCERY_LIST_ACTION = 'add-recipe-to-grocery-list';
+
+    public function __construct(
+        private readonly GetGroceryList $getGroceryList,
+        private readonly AddRecipeToGroceryListHandler $addRecipeToGroceryListHandler,
+    ) {}
+
+    #[Route('/groceries/{id}', name: 'action', methods: ['POST'])]
+    public function __invoke(string $id, Request $request): JsonResponse
+    {
+        $groceryList = $this->getGroceryList->execute($id);
+
+        if (!$groceryList instanceof GroceryList) {
+            throw $this->createNotFoundException('Grocery list not found');
+        }
+
+        $request = $request->toArray();
+
+        $action = $request['groceryListAction'] ?? null;
+
+        if ($action === null) {
+            return new JsonResponse(
+                ["error" => "Missing 'action' parameter in request body"],
+                Response::HTTP_UNPROCESSABLE_ENTITY,
+            );
+        }
+
+        if ($action === self::ADD_RECIPE_TO_GROCERY_LIST_ACTION) {
+            ($this->addRecipeToGroceryListHandler)(new AddRecipeToGroceryListCommand($id, $request['recipeId']));
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse(
+            [
+                'error' => "Unknown action: $action",
+            ],
+            Response::HTTP_FORBIDDEN,
+        );
+    }
+}
