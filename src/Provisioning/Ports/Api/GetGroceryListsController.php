874938d349d850eb1f055e52b994b67cd8eff919
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-05-06 15:15:07 +0200

    Added API GroceryList index endpoint

---
+++ src/Provisioning/Ports/Api/GetGroceryListsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Przper\Tribe\Provisioning\Ports\Api;
+
+use Przper\Tribe\Provisioning\Application\Query\GetGroceryLists;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GetGroceryListsController extends AbstractController
+{
+    public function __construct(
+        private GetGroceryLists $getGroceryListsQuery,
+    ) {
+    }
+
+    #[Route('/groceries', name: 'index', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        return $this->json($this->getGroceryListsQuery->execute());
+    }
+
+}
