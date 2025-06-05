c9d4ee11735d295da1c4443750a46de470bec5e8
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-06-05 13:05:20 +0200

    Add LexikJWTAuthenticationBundle for JWT support
    Integrated LexikJWTAuthenticationBundle to provide JWT-based authentication for the application. Updated dependencies, configuration files, and environment variables to enable and support this feature.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
